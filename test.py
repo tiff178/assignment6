846c50c2f3a742cac7b496aeec389904eb922695
tiff178 <tiff178@gmail.com>
2023-04-25 17:17:42 -0700

    .

--- test.py
+++ test.py
@@ -222,113 +222,113 @@ def flag():
     pygame.draw.line(screen, BRIGHT_YELLOW, [660, 220], [665, 190], 3)
     pygame.draw.polygon(screen, RED, [[668, 190], [675, 196], [665, 205]]) 
 
-def main():
-    done = False
-    while not done:
-        # Event processing (React to key presses, mouse clicks, etc.)
-        ''' for now, we'll just check to see if the X is clicked '''
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_l:
-                    lights_on = not lights_on
-                elif event.key == pygame.K_d:
-                    day = not day
 
-        # Game logic (Check for collisions, update points, etc.)
-        ''' leave this section alone for now ''' 
-        if lights_on:
-            light_color = YELLOW
-        else:
-            light_color = SILVER
+done = False
+while not done:
+    # Event processing (React to key presses, mouse clicks, etc.)
+    ''' for now, we'll just check to see if the X is clicked '''
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_l:
+                lights_on = not lights_on
+            elif event.key == pygame.K_d:
+                day = not day
 
-        if day:
-            sky_color = BLUE
-            field_color = GREEN
-            stripe_color = DAY_GREEN
-            cloud_color = WHITE
-        else:
-            sky_color = DARK_BLUE
-            field_color = DARK_GREEN
-            stripe_color = NIGHT_GREEN
-            cloud_color = NIGHT_GRAY
+    # Game logic (Check for collisions, update points, etc.)
+    ''' leave this section alone for now ''' 
+    if lights_on:
+        light_color = YELLOW
+    else:
+        light_color = SILVER
 
-        for c in clouds:
-            c[0] -= 0.5
+    if day:
+        sky_color = BLUE
+        field_color = GREEN
+        stripe_color = DAY_GREEN
+        cloud_color = WHITE
+    else:
+        sky_color = DARK_BLUE
+        field_color = DARK_GREEN
+        stripe_color = NIGHT_GREEN
+        cloud_color = NIGHT_GRAY
 
-            if c[0] < -100:
-                c[0] = random.randrange(800, 1600)
-                c[1] = random.randrange(0, 150)
-                
-        # Drawing code (Describe the picture. It isn't actually drawn yet.)
-        screen.fill(sky_color)
-        SEE_THROUGH.fill(ck)
-        SEE_THROUGH.set_colorkey(ck)
-        
-        if not day:
-        #stars
-            for s in stars:
-                pygame.draw.ellipse(screen, WHITE, s)
+    for c in clouds:
+        c[0] -= 0.5
 
-        pygame.draw.rect(screen, field_color, [0, 180, 800 , 420])
-        pygame.draw.rect(screen, stripe_color, [0, 180, 800, 42])
-        pygame.draw.rect(screen, stripe_color, [0, 264, 800, 52])
-        pygame.draw.rect(screen, stripe_color, [0, 368, 800, 62])
-        pygame.draw.rect(screen, stripe_color, [0, 492, 800, 82])
-
-        
-        draw_fence()
-
-        for c in clouds:
-            draw_cloud(c[0], c[1])
-        screen.blit(SEE_THROUGH, (0, 0))   
-        
-        boundary_lines()
+        if c[0] < -100:
+            c[0] = random.randrange(800, 1600)
+            c[1] = random.randrange(0, 150)
             
-        
-        score_area()
-        
+    # Drawing code (Describe the picture. It isn't actually drawn yet.)
+    screen.fill(sky_color)
+    SEE_THROUGH.fill(ck)
+    SEE_THROUGH.set_colorkey(ck)
+    
+    if not day:
+    #stars
+        for s in stars:
+            pygame.draw.ellipse(screen, WHITE, s)
 
-        #goal
-        draw_goal()
-            
+    pygame.draw.rect(screen, field_color, [0, 180, 800 , 420])
+    pygame.draw.rect(screen, stripe_color, [0, 180, 800, 42])
+    pygame.draw.rect(screen, stripe_color, [0, 264, 800, 52])
+    pygame.draw.rect(screen, stripe_color, [0, 368, 800, 62])
+    pygame.draw.rect(screen, stripe_color, [0, 492, 800, 82])
 
-        lights()
-            
-        
-        draw_net(320, 338, 360, 5, 3)
-        draw_net(360, 361, 376, 4, 4)
-        draw_net(376, 376, 420, 4, 4)
-        pygame.draw.line(screen, WHITE, [424, 140], [423, 200], 1)
-        draw_net(424, 423, 436, 4, 4)
-        pygame.draw.line(screen, WHITE, [440, 140], [438, 200], 1)
-        draw_net(440, 438, 475, 5, 3)
-        
-        draw_net2()
-        
+    
+    draw_fence()
 
-        stands()
+    for c in clouds:
+        draw_cloud(c[0], c[1])
+    screen.blit(SEE_THROUGH, (0, 0))   
+    
+    boundary_lines()
+        
+    
+    score_area()
     
 
-        flag()
+    #goal
+    draw_goal()
+        
+
+    lights()
+        
+    
+    draw_net(320, 338, 360, 5, 3)
+    draw_net(360, 361, 376, 4, 4)
+    draw_net(376, 376, 420, 4, 4)
+    pygame.draw.line(screen, WHITE, [424, 140], [423, 200], 1)
+    draw_net(424, 423, 436, 4, 4)
+    pygame.draw.line(screen, WHITE, [440, 140], [438, 200], 1)
+    draw_net(440, 438, 475, 5, 3)
+    
+    draw_net2()
     
 
-        # DARKNESS
-        if not day and not lights_on:
-            screen.blit(DARKNESS, (0, 0))    
-        
-        #pygame.draw.polygon(screen, BLACK, [[200, 200], [50,400], [600, 500]], 10)
+    stands()
 
-        ''' angles for arcs are measured in radians (a pre-cal topic) '''
-        #pygame.draw.arc(screen, ORANGE, [100, 100, 100, 100], 0, math.pi/2, 1)
-        #pygame.draw.arc(screen, BLACK, [100, 100, 100, 100], 0, math.pi/2, 50)
 
-        # Update screen (Actually draw the picture in the window.)
-        pygame.display.flip()
+    flag()
 
-        # Limit refresh rate of game loop 
-        clock.tick(refresh_rate)
 
-    # Close window and quit
-    pygame.quit()
+    # DARKNESS
+    if not day and not lights_on:
+        screen.blit(DARKNESS, (0, 0))    
+    
+    #pygame.draw.polygon(screen, BLACK, [[200, 200], [50,400], [600, 500]], 10)
+
+    ''' angles for arcs are measured in radians (a pre-cal topic) '''
+    #pygame.draw.arc(screen, ORANGE, [100, 100, 100, 100], 0, math.pi/2, 1)
+    #pygame.draw.arc(screen, BLACK, [100, 100, 100, 100], 0, math.pi/2, 50)
+
+    # Update screen (Actually draw the picture in the window.)
+    pygame.display.flip()
+
+    # Limit refresh rate of game loop 
+    clock.tick(refresh_rate)
+
+# Close window and quit
+pygame.quit()
